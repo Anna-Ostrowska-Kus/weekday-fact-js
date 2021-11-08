45986518edeffca8d93d833169fd2ea3ae558813
Anna Ostrowska-Kus <anna.ostrowska@wp.pl>
2021-11-08 21:32:18 +0100

    Convert a date to a string

--- script.js
+++ script.js
@@ -13,17 +13,22 @@ const facts = [
 ]
 
 const date = new Date()
-const weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-let day = weekday[date.getDay()]
+// const weekday = new Array(7);
+// weekday[0] = "Sunday";
+// weekday[1] = "Monday";
+// weekday[2] = "Tuesday";
+// weekday[3] = "Wednesday";
+// weekday[4] = "Thursday";
+// weekday[5] = "Friday";
+// weekday[6] = "Saturday";
+// let day = weekday[date.getDay()]
+// currentDay.textContent = day
+let day
+day = new Date(date).toLocaleString('en-us', {weekday:'long'})
 currentDay.textContent = day
 
 let fact = facts [Math.floor(Math.random()*facts.length-1)]
+
+
 funFact.textContent = fact
 
